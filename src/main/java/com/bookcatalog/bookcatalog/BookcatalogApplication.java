26163375c2a1db025583a27df3e5e9b8ac10f6b5
Gihan42 <gihanm729@gmail.com>
2023-06-24 23:27:52 +0530

    first commit

---
+++ src/main/java/com/bookcatalog/bookcatalog/BookcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.bookcatalog.bookcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookcatalog/bookcatalog/BookcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookcatalog.bookcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
